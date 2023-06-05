2fcdb0ab78d4b5950407033c992a5400039cffbe
Gustavo <gustavorsdemelo@gmail.com>
2023-06-04 21:33:12 -0300

    finalizando crud de cliente

--- backend/src/controllers/customer.controller.js
+++ backend/src/controllers/customer.controller.js
@@ -1,6 +1,6 @@
 import customerService from "../services/customer.service.js";
 
-const createCustomer = async (req, res) => {
+const createCustomer = async (req, res) => { // Cadastro de um cliente
     try {
         const { nome } = req.body;
 
@@ -10,9 +10,7 @@ const createCustomer = async (req, res) => {
         const customer = await customerService.createService(req.body);
 
         if (!customer)
-            return res.status(400).send({ message: 'Erro no cadastro do cliente' });
-
-        console.log(customer);    
+            return res.status(400).send({ message: 'Erro no cadastro do cliente' }); 
 
         res.status(201).send({
             customer,
@@ -24,33 +22,59 @@ const createCustomer = async (req, res) => {
     }
 }
 
-const findAllCustomers = async (req, res) => {
+const findAllCustomers = async (req, res) => { // Listagem de todos os clientes cadastrados
     try {
+        const customers = await customerService.findAllService();
 
+        if (customers.length === 0)
+            return res.status(400).send({ message: "Não há clientes cadastrados" });
+        
+        res.send(customers);    
     } catch (err) {
         res.status(500).send({ message: err.message });
     }
 }
 
-const findCustomerById = async (req, res) => {
+const findCustomerById = async (req, res) => {  // Busca de um cliente específico pelo ID
     try {
+        const customer = req.customer;
 
+        res.send(customer);
     } catch (err) {
         res.status(500).send({ message: err.message });
     }
 }
 
-const updateCustomer = async (req, res) => {
+const updateCustomer = async (req, res) => { // Atualiza os campos do cliente
     try {
+        const { nome, telefone, email, divida } = req.body;
 
+        if (!nome && !telefone && !email && !divida)
+            return res.status(400).send({message: "Preencha pelo menos um campo para atualização"});
+
+        const { id } = req;
+
+        await customerService.updateService(
+            id,
+            nome,
+            telefone,
+            email,
+            divida
+        );
+
+        res.send({ message: 'Cliente atualizado com sucesso' });    
     } catch (err) {
         res.status(500).send({ message: err.message });
     }
 }
 
-const deleteCustomer = async (req, res) => {
+const deleteCustomer = async (req, res) => { // Deleta um cliente
     try {
+        const { id } = req;
 
+        await customerService.deleteService(id);
+
+        res.send({ message: 'Cliente deletado com sucesso' });
     } catch (err) {
         res.status(500).send({ message: err.message });
     }
