8c7f5c474eb3d0d68f7366423baaf84215a5d2b5
Gustavo <gustavorsdemelo@gmail.com>
2023-06-10 13:02:48 -0300

    authmiddleware no getallproducts

--- frontend/src/services/postsServices.js
+++ frontend/src/services/postsServices.js
@@ -2,8 +2,20 @@ import axios from "axios";
 
 const baseURL = "http://localhost:8000";
 
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwicGFzc3dvcmQiOiIxMjM0IiwiaWF0IjoxNjg2NDA2MTgyLCJleHAiOjE2ODY2NjUzODJ9._HsxueogdyUT5KsbFmyQHvZT-feMPm-UL9bHOMnQBB0"
+
+const config = {
+    headers: { Authorization: `Bearer ${token}` }
+}
+
 export function getAllPosts() {
-    const response = axios.get(`${baseURL}/produto`);
+    const response = axios.get(`${baseURL}/produto`, config);
 
     return response;
 }
+
+// export function getToken() {
+//     const token = axios.get(`${baseURL}/auth/admin`);
+
+//     return token
+// }
