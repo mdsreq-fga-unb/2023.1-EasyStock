8c5dcab7b0220642d3697be7bfdcd81ef23e1483
Gustavo <gustavorsdemelo@gmail.com>
2023-06-10 15:09:55 -0300

    auth com react-hook

--- frontend/src/services/authServices.js
+++ frontend/src/services/authServices.js
@@ -2,6 +2,12 @@ import axios from "axios";
 
 const baseURL = "http://localhost:8000";
 
+export function login(data) {
+    const response = axios.post(`${baseURL}/auth/admin`, data);
+
+    return response;
+}
+
 export function getToken() {
     const token = axios.get(`${baseURL}/auth/admin`);
 
