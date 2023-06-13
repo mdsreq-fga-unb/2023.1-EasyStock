f147a2fb04f5bb069d18cf524290b632f01fb49d
Gustavo <gustavorsdemelo@gmail.com>
2023-06-12 22:04:52 -0300

    ajustando vite pro deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,8 @@
+import pluginRewriteAll from 'vite-plugin-rewrite-all';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), pluginRewriteAll()],
 })
