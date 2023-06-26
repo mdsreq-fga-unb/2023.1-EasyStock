ef555c951f44b180660540f0a43ae1a49d5986b7
Daniela <danielaalarcao21@gmail.com>
2023-06-26 16:41:01 -0300

    Teste customer completo

--- backend/test/auth.test.js
+++ backend/test/auth.test.js
@@ -5,18 +5,18 @@ import jwt from 'jsonwebtoken';
 
 describe('Testando operações de Token', () => {
     it('Testando geração de Token', () => {
-        //Dados de teste
+
         const username = 'Carla';
         const password = 'Carla123';
 
-        //Gerar Token
+
         const token = generateToken(username, password);
 
-        //Verificando se o token foi gerado corretamente
+
         expect(token).toBeDefined();
         expect(typeof token).toBe('string')
 
-        //Verificar se o token pode ser verificado corretamente
+        
         const decodedToken = jwt.verify(token, process.env.SECRET_JWT);
         expect(decodedToken).toBeDefined();
         expect(decodedToken.username).toBe(username);
