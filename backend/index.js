92d0b96cec6fde202384080381fc314e76a99369
Gustavo <gustavorsdemelo@gmail.com>
2023-07-18 07:10:59 -0300

    teste

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 const PORT = process.env.PORT || 8000;
 
 app.use(cors({
-    origin: "http://localhost:5173"
+    origin: "https://easystock-app.netlify.app/"
 }))
 
 app.use("/produto", productRoute); // Rota padrão de produto
